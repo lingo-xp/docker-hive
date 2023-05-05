8bc0a32a5f13640246db8883b50cd7e87bf2e030
lingo <xiaopeng@dipeak.com>
2023-05-05 22:24:19 +0800

    modify images

--- startup.sh
+++ startup.sh
@@ -6,4 +6,5 @@ hadoop fs -chmod g+w   /tmp
 hadoop fs -chmod g+w   /user/hive/warehouse
 
 cd $HIVE_HOME/bin
+kinit -k -t /server.keytab server/server-host@DIPEAK.COM
 ./hiveserver2 --hiveconf hive.server2.enable.doAs=false
