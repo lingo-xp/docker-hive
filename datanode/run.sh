3a00723f117f61b5f07538696e66930aa1e3d987
lingo <xiaopeng@dipeak.com>
2023-05-06 21:01:58 +0800

    modify images

--- datanode/run.sh
+++ datanode/run.sh
@@ -6,6 +6,7 @@ if [ ! -d $datadir ]; then
   exit 2
 fi
 
+while :; do  sleep 10; done
 
-kinit -k -t /keys/datanode.keytab hdfs/datanode@DIPEAK.COM
-$HADOOP_HOME/bin/hdfs --config $HADOOP_CONF_DIR datanode
+#kinit -k -t /keys/datanode.keytab hdfs/datanode@DIPEAK.COM
+#$HADOOP_HOME/bin/hdfs --config $HADOOP_CONF_DIR datanode
